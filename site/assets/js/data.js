a0ed97016e4b6369802193691badf01a14b2eb08
Ohad Shpindel <ohadshpindel@gmail.com>
2026-04-05 22:50:25 +0300

    Make all images permanent: extract embedded + KV content to static files
    - Extract 4 embedded base64 images to static files on disk:
      gaming/gaming_room_final.jpg, kitchen/kitchen_room_final.jpg,
      camping/camping_room_final.jpg, home-office/home-office-v1.png
    - Merge KV product data (32 products across 4 categories) into data.js
    - data.js now has 7 categories, 13 images, all products preserved
    - Empty EMBEDDED_IMAGES in Worker (17KB instead of 12.7MB)
    - Redirect /api/images to static files in both Worker files
    - All content is now in git = permanent and free

--- site/assets/js/data.js
+++ site/assets/js/data.js
@@ -178,6 +178,83 @@ const SITE_DATA = {
                     "title": "מטבח מודרני מאובזר",
                     "description": "מטבח מעוצב עם כלים וגאדג׳טים לרכישה מ-AliExpress",
                     "products": []
+                },
+                {
+                    "file": "kitchen_room_final.jpg",
+                    "title": "מטבח מודרני ומעוצב",
+                    "description": "מטבח מאובזר במוצרים איכותיים מאליאקספרס לשיפור חווית הבישול והאירוח.",
+                    "products": [
+                        {
+                            "name": "מנורת תליון LED מוזהבת",
+                            "price": "₪240.70",
+                            "url": "https://he.aliexpress.com/item/1005006966366435.html",
+                            "icon": "💡",
+                            "coords": {
+                                "x": 50,
+                                "y": 20
+                            }
+                        },
+                        {
+                            "name": "כסאות בר נורדיים (זוג)",
+                            "price": "₪725.83",
+                            "url": "https://he.aliexpress.com/item/1005010640884262.html",
+                            "icon": "🪑",
+                            "coords": {
+                                "x": 40,
+                                "y": 75
+                            }
+                        },
+                        {
+                            "name": "אייר פרייר 5 ליטר",
+                            "price": "₪297.26",
+                            "url": "https://he.aliexpress.com/item/1005010207199904.html",
+                            "icon": "🍳",
+                            "coords": {
+                                "x": 75,
+                                "y": 55
+                            }
+                        },
+                        {
+                            "name": "קומקום חשמלי Midea",
+                            "price": "₪118.45",
+                            "url": "https://he.aliexpress.com/item/1005010113215929.html",
+                            "icon": "🫖",
+                            "coords": {
+                                "x": 20,
+                                "y": 60
+                            }
+                        },
+                        {
+                            "name": "טוסטר 2 פרוסות",
+                            "price": "₪87.55",
+                            "url": "https://he.aliexpress.com/item/1005008519687981.html",
+                            "icon": "🍞",
+                            "coords": {
+                                "x": 30,
+                                "y": 60
+                            }
+                        },
+                        {
+                            "name": "סט סכינים מקצועי 7 חלקים",
+                            "price": "₪89.78",
+                            "url": "https://he.aliexpress.com/item/1005007358970951.html",
+                            "icon": "🔪",
+                            "coords": {
+                                "x": 60,
+                                "y": 60
+                            }
+                        },
+                        {
+                            "name": "מעמד תבלינים 3 קומות",
+                            "price": "₪83.09",
+                            "url": "https://he.aliexpress.com/item/1005009278474082.html",
+                            "icon": "🧂",
+                            "coords": {
+                                "x": 85,
+                                "y": 60
+                            }
+                        }
+                    ]
                 }
             ]
         },
@@ -197,6 +274,228 @@ const SITE_DATA = {
                     "title": "תחנת מזג אוויר ביתית",
                     "description": "גאדג׳ט מזג אוויר חכם לבית מ-AliExpress",
                     "products": []
+                }
+            ]
+        },
+        {
+            "id": "gaming",
+            "label": "גיימינג",
+            "icon": "🎮",
+            "images": [
+                {
+                    "file": "gaming_room_final.jpg",
+                    "title": "חדר גיימינג מודרני עם מוצרי אליאקספרס",
+                    "description": "כל המוצרים אמיתיים וזמינים באליאקספרס. חדר גיימינג מעוצב לשיפור חווית המשחק.",
+                    "products": [
+                        {
+                            "name": "מקלדת מכנית RK RKG68",
+                            "price": "₪128.44",
+                            "url": "https://he.aliexpress.com/item/1005003716446397.html",
+                            "icon": "⌨️",
+                            "coords": {
+                                "x": 45,
+                                "y": 65
+                            }
+                        },
+                        {
+                            "name": "עכבר Razer Basilisk V3",
+                            "price": "₪153.63",
+                            "url": "https://he.aliexpress.com/item/1005010650792010.html",
+                            "icon": "🖱️",
+                            "coords": {
+                                "x": 60,
+                                "y": 68
+                            }
+                        },
+                        {
+                            "name": "אוזניות ONIKUMA X25",
+                            "price": "₪69.82",
+                            "url": "https://he.aliexpress.com/item/1005008196060222.html",
+                            "icon": "🎧",
+                            "coords": {
+                                "x": 30,
+                                "y": 55
+                            }
+                        },
+                        {
+                            "name": "מנורות קיר משושות RGB",
+                            "price": "₪77.10",
+                            "url": "https://he.aliexpress.com/item/1005006275326644.html",
+                            "icon": "💡",
+                            "coords": {
+                                "x": 50,
+                                "y": 25
+                            }
+                        },
+                        {
+                            "name": "משטח עכבר RGB ענק",
+                            "price": "₪48.81",
+                            "url": "https://he.aliexpress.com/item/1005003823347076.html",
+                            "icon": "⬛",
+                            "coords": {
+                                "x": 50,
+                                "y": 70
+                            }
+                        },
+                        {
+                            "name": "מעמד לבקר ואוזניות",
+                            "price": "₪24.36",
+                            "url": "https://he.aliexpress.com/item/1005007096432815.html",
+                            "icon": "🎮",
+                            "coords": {
+                                "x": 70,
+                                "y": 60
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id": "camping",
+            "label": "קמפינג וטיולים",
+            "icon": "🏕️",
+            "images": [
+                {
+                    "file": "camping_room_final.jpg",
+                    "title": "קמפינג וטיולים בטבע",
+                    "description": "ציוד קמפינג איכותי ומודרני לחוויית שטח מושלמת.",
+                    "products": [
+                        {
+                            "name": "כירה אינפרא אדומה BISINNA",
+                            "price": "₪144.13",
+                            "url": "https://he.aliexpress.com/item/1005009594126670.html",
+                            "icon": "🔥",
+                            "coords": {
+                                "x": 45,
+                                "y": 75
+                            }
+                        },
+                        {
+                            "name": "מכונת אספרסו ניידת",
+                            "price": "₪121.72",
+                            "url": "https://he.aliexpress.com/item/1005009460471862.html",
+                            "icon": "☕",
+                            "coords": {
+                                "x": 55,
+                                "y": 70
+                            }
+                        },
+                        {
+                            "name": "עששית קמפינג רטרו",
+                            "price": "₪30.00",
+                            "url": "https://he.aliexpress.com/item/1005006727508946.html",
+                            "icon": "🏮",
+                            "coords": {
+                                "x": 35,
+                                "y": 60
+                            }
+                        },
+                        {
+                            "name": "שולחן קמפינג מתקפל",
+                            "price": "₪74.64",
+                            "url": "https://he.aliexpress.com/item/1005005653995402.html",
+                            "icon": "🪑",
+                            "coords": {
+                                "x": 50,
+                                "y": 80
+                            }
+                        },
+                        {
+                            "name": "תיק אחסון טקטי",
+                            "price": "₪47.95",
+                            "url": "https://he.aliexpress.com/item/1005009045188656.html",
+                            "icon": "🎒",
+                            "coords": {
+                                "x": 70,
+                                "y": 65
+                            }
+                        },
+                        {
+                            "name": "סט סירים CAMPINGMOON",
+                            "price": "₪47.16",
+                            "url": "https://he.aliexpress.com/item/1005007353164081.html",
+                            "icon": "🍲",
+                            "coords": {
+                                "x": 40,
+                                "y": 70
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id": "home-office",
+            "label": "משרד ביתי",
+            "icon": "💼",
+            "images": [
+                {
+                    "file": "home-office-v1.png",
+                    "title": "משרד ביתי מודרני ומעוצב",
+                    "description": "סביבת עבודה ארגונומית ומעוררת השראה לשיפור הפרודוקטיביות בבית.",
+                    "products": [
+                        {
+                            "name": "שולחן עבודה קומפקטי מודרני",
+                            "price": "₪561.31",
+                            "url": "https://www.aliexpress.com/item/1005008519687981.html",
+                            "icon": "💻",
+                            "coords": {
+                                "x": 50,
+                                "y": 75
+                            }
+                        },
+                        {
+                            "name": "כיסא משרדי ארגונומי",
+                            "price": "₪665.27",
+                            "url": "https://www.aliexpress.com/item/1005008196060222.html",
+                            "icon": "🪑",
+                            "coords": {
+                                "x": 15,
+                                "y": 80
+                            }
+                        },
+                        {
+                            "name": "מעמד למסך עם מגירה",
+                            "price": "₪238.58",
+                            "url": "https://www.aliexpress.com/item/1005009481102235.html",
+                            "icon": "🖥️",
+                            "coords": {
+                                "x": 55,
+                                "y": 55
+                            }
+                        },
+                        {
+                            "name": "מנורת שולחן ספירלה LED",
+                            "price": "₪36.25",
+                            "url": "https://www.aliexpress.com/item/1005009692606690.html",
+                            "icon": "💡",
+                            "coords": {
+                                "x": 32,
+                                "y": 45
+                            }
+                        },
+                        {
+                            "name": "משטח שולחן מעור PU",
+                            "price": "₪23.84",
+                            "url": "https://www.aliexpress.com/item/1005007096432815.html",
+                            "icon": "⬛",
+                            "coords": {
+                                "x": 50,
+                                "y": 65
+                            }
+                        },
+                        {
+                            "name": "נורת ריחוף מגנטית",
+                            "price": "₪138.84",
+                            "url": "https://www.aliexpress.com/item/1005006727508946.html",
+                            "icon": "🪄",
+                            "coords": {
+                                "x": 75,
+                                "y": 45
+                            }
+                        }
+                    ]
                 }
             ]
         },
